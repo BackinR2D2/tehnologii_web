b2a3c506afaf2b0dd9980925aff586a7008822a3
Cristian Niculae <cristimlg334@gmail.com>
2025-01-06 16:01:44 +0200

    debug & updated prod urls

--- client/src/utils/url.js
+++ client/src/utils/url.js
@@ -1,4 +1,7 @@
-export const url =
-	process.env.NODE_ENV === 'development'
-		? 'http://localhost:5000/api'
-		: 'https://tehnologii-web.onrender.com/api';
+export let url = '';
+if (import.meta.env.MODE === 'development') {
+	url = 'http://localhost:5000/api';
+} else {
+	url = 'https://tehnologii-web.onrender.com/api';
+}
+console.log(url, import.meta.env);
